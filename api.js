d11d345f3243e337c52b1e652fa356907feb7eee
Prithivraj005 <005prithivraj@gmail.com>
2025-11-09 10:03:14 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,33 @@
+const API_BASE_URL = "https://three65-82mp.onrender.com"; // change later to deployed URL
+export default API_BASE_URL;
+
+
+export async function signup(username, password) {
+  const res = await fetch(`${API_BASE_URL}/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.error || "Signup failed");
+  }
+
+  return res.json();
+}
+
+export async function login(username, password) {
+  const res = await fetch(`${API_BASE_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.error || "Login failed");
+  }
+
+  return res.json();
+}
